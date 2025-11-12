cfef80653290d54beea600791d74bbd00a9da668
B00165442 Tobby Akinwale <B00165442@mytudublin.ie>
2025-11-12 10:04:54 +0000

    completed part one

---
+++ CarPolicy.php
@@ -0,0 +1,46 @@
+<?php
+class CarPolicy
+{
+      private $policyNumber="";
+      private $yearlyPremium=0;
+      private $dateofLastClaim="";
+
+      public function __construct($pn, $yp)
+      {
+
+         $this->policyNumber=$pn;
+         $this->yearlyPremium=$yp;
+      }
+      public function setDateofLastClaim($dolc)
+      {
+        $this->setDateofLastClaim=$dolc;
+
+      }
+
+      public function getTotalYearsNoClaims()
+      {
+
+        $currentDate = new DateTime();
+        $lastDate= new DateTime($this->dateOfLastClaim);
+        $interval = $currentDate->diff($lastDate);
+        return $interval->format("%y");
+
+
+      }
+       public function __toString()
+       {
+
+        return "PN:" . $this->policyNumber;
+       }
+     
+
+      
+    
+
+
+      
+}
+
+
+
+?>
